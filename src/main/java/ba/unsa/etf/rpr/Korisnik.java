1ce5e9f1d2d69b65e3766d9743579468f929e739
your name <you@example.com>
2023-10-31 11:49:17 +0100

    lv2z2

---
+++ src/main/java/ba/unsa/etf/rpr/Korisnik.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr;
+
+public class Korisnik extends Osoba {
+    public Racun racun;
+
+    public Korisnik(String ime, String prezime){
+        super(ime,prezime);
+    }
+
+    public void dodajRacun(Racun racun){
+
+    }
+}
