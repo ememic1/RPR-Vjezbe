0ef7de71c34c2d924cec9d4ce22dba126dcfe3da
your name <you@example.com>
2023-10-31 16:02:33 +0100

    lv2z1

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -4,10 +4,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-            System.out.print("Unesite broj: ");
-            Scanner unos = new Scanner(System.in);
-            int broj = unos.nextInt();
-            System.out.println("Faktorijel broja je " + Faktorijel.factorial(broj));
-            System.out.println("Sinus broja je " + Sinus.sin(broj));
+            if(args.length == 0 ){
+                System.out.println("Unesite broj: ");
+                Scanner unos = new Scanner(System.in);
+                int broj = unos.nextInt();
+                System.out.println("Faktorijel broja je " + Faktorijel.factorial(broj));
+                System.out.println("Sinus broja je " + Sinus.sin(broj));
+            }
+            else {
+                if(args[0].equals("sin")) System.out.println(Sinus.sin(Integer.parseInt(args[1])));
+                else System.out.println(Faktorijel.factorial(Integer.parseInt(args[1])));
+            }
     }
 }
